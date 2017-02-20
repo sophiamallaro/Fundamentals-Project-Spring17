04eac771b054f054f5e8ead33d7b6106a0fae1bb
Jon Tessmann <jonathon-tessmann@uiowa.edu>
2017-02-20 14:05:11 -0600

    fixed some php errors, did some file structuring

--- Team9ARS/pages/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'psl-config.php';
-
-$mysqli = new mysqli(HOST,USER,PASSWORD,DATABASE);
-
-?>
---
+++ Team9ARS/www/includes/psl-config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* database login details
+*/
+define("HOST", "localhost");
+define("USER", "sec_user");
+define("PASSWORD" , "Team9password");
+define("DATABASE" , "ARS");
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", FALSE); //DEVELOPMENT SETTING ONLY!!
+?>
