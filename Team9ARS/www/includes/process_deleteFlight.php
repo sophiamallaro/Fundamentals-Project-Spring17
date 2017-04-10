322605fa68da06d1da5d5520736707e9c8bd89b7
smallaro <sophia-mallaro@uiowa.edu>
2017-04-09 20:06:17 -0500

    being responsible and committing.

---
+++ Team9ARS/www/includes/process_deleteFlight.php
@@ -0,0 +1,68 @@
+<?php
+include_once 'db_connect.php';
+include_once 'functions.php';
+
+sec_session_start(); // Our custom secure way of starting a PHP session.
+
+ini_set("date.timezone", "America/New_York");
+
+$number = $_POST['number'];
+
+$prep_stmt = "SELECT * FROM flight WHERE number = ? LIMIT 1";
+$stmt = $mysqli->prepare($prep_stmt);
+
+// check if such a flight exists
+if ($stmt) {
+    $stmt->bind_param('s', $number);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows == 0) {
+
+        $error_msg .= '<p class="error">No flight with this number exists.</p>';
+                        $stmt->close();
+
+
+    }
+    else {
+        $ps = "SELECT * FROM flight WHERE number = ? AND firstClass = 0 AND economy = 0 LIMIT 1";
+        $st = $mysqli->prepare($ps);
+        if($st) {
+            $st->bind_param('s', $number);
+            $st->execute();
+            $st->store_result();
+
+            if($st->num_rows==0) {
+                $error_msg .= '<p class="error">This flight is booked and cannot be cancelled.</p>';
+                $st->close();
+            } else {
+                $prep_statement = "DELETE FROM flight WHERE number = ?";
+                  $statement = $mysqli->prepare($prep_statement);
+                  if($statement) {
+                            $statement->bind_param('s', $number);
+                            $statement->execute();
+                  } else {
+                   $error_msg .= 'I AM NOT PREPARING PROPERLY';
+                  }
+
+            }
+
+        }
+
+    }
+
+} else {
+    $error_msg .= 'MAIN STATEMENT NOT PREPARING PROPERLY';
+
+}
+
+if(empty($error_msg)) {
+    header('Location: ../pages/adminTools.html');
+    exit();
+} else {
+    header('Location: ../pages/deleteFlight.html');
+    exit();
+}
+
+
+ ?>
--- Team9ARS/www/pages/flightSearch.php
+++ Team9ARS/www/pages/flightSearch.php
@@ -13,53 +13,37 @@ include_once '../includes/functions.php';
     <div style='float: right;'><a href="../index.php">Home</a></div>
 </head>
 <body>
-<h1>Search Flights</h1>
-<?php
-        if (!empty($error_msg)) {
-            echo $error_msg;
+    <h1>Search Flights</h1>
+
+	<form action="../includes/process_search.php" method="post"
+	name="aircraft_form" id="aircraft_form">
+
+	<?php
+		$stmt = $mysqli->query("SELECT * FROM flight");
+
+        if ($stmt->num_rows > 0) {
+            while($row = $stmt->fetch_assoc()) {
+
+                echo '<tr>';
+                '<td>'.$row['number'].'</td>';
+                '<td>'.$row['ddate'].'</td>';
+                '<td>'.$row['adate'].'</td>';
+                '<td>'.$row['dtime'].'</td>';
+                '<td>'.$row['atime'].'</td>';
+                '<td>'.$row['ePrice'].'</td>';
+                '<td>'.$row['fcPrice'].'</td>';
+
+                echo '</tr>';
+                }
+                       // echo "</select>";
+        } else {
+            echo 'I am not working.';
         }
-    ?>
-<form action="../includes/process_search.php" method="post" name="test_form">
-    From: <input type="text" name="source">
-    To: <input type="text" name="destination"><br>
-    Departure Date: <input type="date" name="ddate"><br>
-    Return Date: <input type="date" name="adate"><br>
-    <input type="submit" value="Search"><br>
 
-</form>
-
-<table>
-    <thead>
-    <tr>
-        <td>From</td>
-        <td>To</td>
-        <td>Departure Date</td>
-        <td>Arrival Date</td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-            $connect = mysql_connect("localhost","root", "root");
-            if (!$connect) {
-                die(mysql_error());
-            }
-            mysql_select_db("apploymentdevs");
-            $results = mysql_query("SELECT * FROM demo LIMIT 10");
-            while($row = mysql_fetch_array($results)) {
-            ?>
-    <tr>
-        <td><?php echo $row['Id']?></td>
-        <td><?php echo $row['Name']?></td>
-    </tr>
-
-    <?php
-            }
-            ?>
-    </tbody>
-</table>
-</body>
-</html>
-
+	?>
+            <input type="submit"
+                   value="Book" />
+    </form>
 
 </body>
 </html>
