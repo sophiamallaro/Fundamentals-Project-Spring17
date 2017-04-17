8be9773d0779a1a36761e0083180d3375888352f
Jason Ryan <jason.w.ryan9@gmail.com>
2017-04-16 19:00:13 -0500

    Broke into mapping in separate functions.

--- Team9ARS/www/js/maps.js
+++ Team9ARS/www/js/maps.js
@@ -3,7 +3,6 @@
  */
 
 var map;
-var flightPath;
 var atl = {lat: 33.6407, lng: -84.4277};
 var cid = {lat: 41.8864, lng: -91.70697};
 var jfk = {lat: 40.6413, lng: -73.7781};
@@ -11,48 +10,54 @@ var ord = {lat: 41.9742, lng: -87.9072};
 var sfo = {lat: 37.7749, lng: -122.4194};
 
 var airports = {'atl':atl, 'cid':cid, 'jfk':jfk, 'ord':ord, 'sfo':sfo};
-//var flightPlanCoordinates = [airports['cid'], airports['ord'], airports['jfk']];
-var flightPlanCoordinates = generateFilghtPlan([airports['cid'], airports['ord'], airports
-['jfk']]);
+var flightPlanCoordinates = generateFlightPlan(['cid', 'jfk']);
 var center = calcCenter(flightPlanCoordinates);
+center = {lat: 39.8282, lng: -98.5795};
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
-        zoom: 5
+        zoom: 5,
+        mapTypeId: 'terrain'
     });
 
+    addMkrs(flightPlanCoordinates, map);
+    addFlightPath(flightPlanCoordinates, map, '#FF0000');
+}
+
+// Adds flight path to map based on flight plan
+function addFlightPath(flightPlan, map, color) {
+    var flightPath;
     flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
+        path: flightPlan,
         geodesic: true,
-        strokeColor: '#FF0000',
         strokeOpacity: 0.8,
-        strokeWeight: 4
+        strokeWeight: 4,
+        strokeColor: color
     });
-
-    addMkrs(flightPlanCoordinates, ['1', '2', '3'], map);
 
     flightPath.setMap(map);
 }
 
+
 //Generates flight plan
-function generateFilghtPlan(locations) {
+function generateFlightPlan(locations) {
     var index, len;
     var flightPlanCoordinates = [];
     for(index = 0, len = locations.length; index < len; ++index) {
-        flightPlanCoordinates.push(locations[index]);
+        flightPlanCoordinates.push(airports[locations[index]]);
     }
     return flightPlanCoordinates;
 }
 
 // Function adds Marker to map with label
-function addMkrs(locations, names, map) {
+function addMkrs(locations, map) {
     var mkr;
     var index, len;
     for(index = 0, len = locations.length; index < len; ++index) {
         mkr = new google.maps.Marker({
             position: locations[index],
-            label: names[index],
             map: map
         });
     }
