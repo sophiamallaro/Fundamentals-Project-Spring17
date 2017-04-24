d69eb35c7d0d5010f1a540034ff46f470df191aa
smallaro <sophia-mallaro@uiowa.edu>
2017-04-24 13:52:03 -0500

    sort of trashy double flight search working

--- Team9ARS/www/pages/flightSearch.php
+++ Team9ARS/www/pages/flightSearch.php
@@ -18,11 +18,14 @@ sec_session_start();
 </head>
 <body>
     <h1>Results</h1>
+	<h2>Direct Flights</h2>
 	<form action="booking.php" method="post"
 	name="aircraft_form" id="aircraft_form">
     <table width="1000">
     <tr>
         <td>Flight Number</td>
+		<td>Departure Destination</td>
+        <td>Arrival Destination</td>
         <td>Departure Date</td>
         <td>Arrival Date</td>
         <td>Departure Time</td>
@@ -58,6 +61,8 @@ sec_session_start();
                         while($row = $stmt->fetch_assoc()) {
                             echo '<tr>';
                             echo '<td>'.$row['number'].'</td>';
+							echo '<td>'.$row['source'].'</td>';
+							echo '<td>'.$row['destination'].'</td>';
                             echo '<td>'.$row['ddate'].'</td>';
                             echo '<td>'.$row['adate'].'</td>';
                             echo '<td>'.$row['dtime'].'</td>';
@@ -78,6 +83,129 @@ sec_session_start();
 
 	?>
 	</table><br>
+	
+	<h2>First Leg</h2>
+	<form action="booking.php" method="post"
+	name="aircraft_form" id="aircraft_form">
+    <table width="1000">
+    <tr>
+        <td>Flight Number</td>
+        <td>Departure Date</td>
+        <td>Arrival Date</td>
+        <td>Departure Time</td>
+        <td>Arrival Time</td>
+        <td>Economy Price</td>
+        <td>First Class Price</td>
+    </tr>
+		<?php
+	    $source = $_POST['source'];
+	    $destination = $_POST['destination'];
+        $insertddate = date('Y-m-d', strtotime($_POST['ddate']));
+		$sort = $_POST['sort'];
+		
+		if($sort == 0) {
+			//$in = "SELECT * FROM flight ORDER BY ePrice";
+			$instruction = "SELECT * FROM flight where source='".$source."' AND destination!='".$destination."' AND ddate ='".$insertddate."' ORDER BY ePrice";
+		} else if($sort == 1) {
+			$instruction = "SELECT * FROM flight where source='".$source."' AND destination!='".$destination."' AND ddate ='".$insertddate."' ORDER BY dTime";
+			//$in = "SELECT * FROM flight ORDER BY dtime";
+			//$s = $mysqli->prepare($in);
+			//$s->execute();
+		} else {
+			$instruction = "SELECT * FROM flight where source='".$source."' AND destination!='".$destination."' AND ddate ='".$insertddate."' ORDER BY aTime";
+			//$in = "SELECT * FROM flight ORDER BY atime";
+			//$s = $mysqli->prepare($in);
+			//$s->execute();
+		}
+		
+        //$instruction = "SELECT * FROM flight where source='".$source."' AND destination='".$destination."' AND ddate ='".$insertddate."'";
+		$stmt = $mysqli->query($instruction);
+        if($stmt) {
+            if ($stmt->num_rows > 0) {
+                        while($row = $stmt->fetch_assoc()) {
+                            echo '<tr>';
+                            echo '<td>'.$row['number'].'</td>';
+							echo '<td>'.$row['source'].'</td>';
+							echo '<td>'.$row['destination'].'</td>';
+                            echo '<td>'.$row['ddate'].'</td>';
+                            echo '<td>'.$row['adate'].'</td>';
+                            echo '<td>'.$row['dtime'].'</td>';
+                            echo '<td>'.$row['atime'].'</td>';
+                            echo '<td>'.$row['ePrice'].'</td>';
+                            echo '<td>'.$row['fcPrice'].'</td>';
+							echo '<td><button type="submit" name ="number" value="'.$row['number'].'">Book Flight '.$row['number'].'</button></td>';
+							echo '<td><a href="../index.php">/View Map</a></td>';
+                            echo '</tr>';
+                        }
+                        // echo "</select>";
+            } else {
+                echo 'No Available Flights';
+            }
+        } else {
+                echo 'Error';
+        }
+
+	?>
+	</table><br>
+	
+	<h2>Second Leg</h2>
+	<form action="booking.php" method="post"
+	name="aircraft_form" id="aircraft_form">
+    <table width="1000">
+    <tr>
+        <td>Flight Number</td>
+        <td>Departure Date</td>
+        <td>Arrival Date</td>
+        <td>Departure Time</td>
+        <td>Arrival Time</td>
+        <td>Economy Price</td>
+        <td>First Class Price</td>
+    </tr>
+		<?php
+	    $source = $_POST['source'];
+	    $destination = $_POST['destination'];
+        $insertddate = date('Y-m-d', strtotime($_POST['ddate']));
+		$sort = $_POST['sort'];
+		
+		if($sort == 0) {
+			$instruction = "SELECT * FROM flight where source!='".$source."' AND destination='".$destination."' AND ddate ='".$insertddate."' ORDER BY ePrice";
+		} else if($sort == 1) {
+			$instruction = "SELECT * FROM flight where source!='".$source."' AND destination='".$destination."' AND ddate ='".$insertddate."' ORDER BY dTime";
+		} else {
+			$instruction = "SELECT * FROM flight where source!='".$source."' AND destination='".$destination."' AND ddate ='".$insertddate."' ORDER BY aTime";
+		}
+		
+        //$instruction = "SELECT * FROM flight where source='".$source."' AND destination='".$destination."' AND ddate ='".$insertddate."'";
+		$stmt = $mysqli->query($instruction);
+        if($stmt) {
+            if ($stmt->num_rows > 0) {
+                        while($row = $stmt->fetch_assoc()) {
+                            echo '<tr>';
+                            echo '<td>'.$row['number'].'</td>';
+							echo '<td>'.$row['source'].'</td>';
+							echo '<td>'.$row['destination'].'</td>';
+                            echo '<td>'.$row['ddate'].'</td>';
+                            echo '<td>'.$row['adate'].'</td>';
+                            echo '<td>'.$row['dtime'].'</td>';
+                            echo '<td>'.$row['atime'].'</td>';
+                            echo '<td>'.$row['ePrice'].'</td>';
+                            echo '<td>'.$row['fcPrice'].'</td>';
+							echo '<td><button type="submit" name ="number" value="'.$row['number'].'">Book Flight '.$row['number'].'</button></td>';
+							echo '<td><a href="../index.php">/View Map</a></td>';
+                            echo '</tr>';
+                        }
+                        // echo "</select>";
+            } else {
+                echo 'No Available Flights';
+            }
+        } else {
+                echo 'Error';
+        }
+
+	?>
+	</table><br>
+	
+
     </form>
 </body>
 </html>
